d59eeba1168e8d3e4b42c78a668c4ca6ccc5b535
jguachuns <jguachuns@unemi.edu.ec>
2023-12-06 22:28:43 -0500

    .

--- core/models.py
+++ core/models.py
@@ -54,22 +54,22 @@ class ModeloBaseAdmin(admin.ModelAdmin):
                 del actions['delete_selected']
         return actions
 
-    def has_add_permission(self, request):
-        return request.user.username in [x[0] for x in MANAGERS]
+    # def has_add_permission(self, request):
+    #     return request.user.username in [x[0] for x in MANAGERS]
 
-    def has_change_permission(self, request, obj=None):
-        return True
+    # def has_change_permission(self, request, obj=None):
+    #     return True
 
-    def has_delete_permission(self, request, obj=None):
-        return request.user.username in [x[0] for x in MANAGERS]
+    # def has_delete_permission(self, request, obj=None):
+    #     return request.user.username in [x[0] for x in MANAGERS]
 
     def get_form(self, request, obj=None, **kwargs):
         self.exclude = ("usuario_creacion", "fecha_creacion", "usuario_modificacion", "fecha_modificacion")
         form = super(ModeloBaseAdmin, self).get_form(request, obj, **kwargs)
         return form
 
-    def save_model(self, request, obj, form, change):
-        if request.user.username not in [x[0] for x in MANAGERS]:
-             raise Exception('Sin permiso a modificacion')
-        else:
-            obj.save(request)
+    # def save_model(self, request, obj, form, change):
+    #     if request.user.username not in [x[0] for x in MANAGERS]:
+    #          raise Exception('Sin permiso a modificacion')
+    #     else:
+    #         obj.save(request)
